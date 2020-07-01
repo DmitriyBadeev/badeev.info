c58bcde3ba5a5c7040402c1848311bc206273254
DmitryBadeev <dmitriibadeev@gmail.com>
2020-07-01 19:23:40 +0500

    fix router and avatar update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from "react"
-import { Switch, BrowserRouter, Route } from "react-router-dom"
+import React, { useEffect } from "react"
+import { Switch, BrowserRouter, Route, useLocation } from "react-router-dom"
 import { ApolloProvider } from "@apollo/react-hooks"
 import ApolloClient from "apollo-boost"
 import Shared from "./pages/shared/Shared"
@@ -17,9 +17,11 @@ const App: React.FC = () => {
             <BrowserRouter>
                 <Shared>
                     <Switch>
-                        <Route path="/" component={MainPage} exact />
-                        <Route path="/portfolio" component={PortfolioPage} exact />
-                        <Route path="/portfolio/:id" component={WorkPage} />
+                        <ScrollToTop>
+                            <Route path="/" component={MainPage} exact />
+                            <Route path="/portfolio" component={PortfolioPage} exact />
+                            <Route path="/portfolio/:id" component={WorkPage} />
+                        </ScrollToTop>
                     </Switch>
                 </Shared>
             </BrowserRouter>
@@ -28,3 +30,13 @@ const App: React.FC = () => {
 }
 
 export default App
+
+const ScrollToTop: React.FC = (props) => {
+    let location = useLocation()
+
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [location.pathname])
+
+    return <>{props.children}</>
+}
