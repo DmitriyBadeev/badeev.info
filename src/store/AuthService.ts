68cd3698923e2941a1bb6e85606ed22c96c0ef48
DmitryBadeev <dmitriibadeev@gmail.com>
2020-07-16 22:13:01 +0500

    to code flow

--- src/store/AuthService.ts
+++ src/store/AuthService.ts
@@ -1,16 +1,37 @@
-import { UserManager, UserManagerSettings, User } from "oidc-client"
+import { UserManager, UserManagerSettings, User, WebStorageStateStore } from "oidc-client"
 import { action, observable } from "mobx"
 
-const config: UserManagerSettings = {
+const prodConfig: UserManagerSettings = {
     authority: "https://identity.badeev.info",
     client_id: "spa_badeev",
     redirect_uri: "https://badeev.info/auth-complete",
-    response_type: "id_token token",
+    response_type: "code",
     scope: "openid Portfolio.API",
     post_logout_redirect_uri: "https://badeev.info",
+    userStore: new WebStorageStateStore({ store: window.localStorage }),
 }
 
-const userManager = new UserManager(config)
+const devConfig: UserManagerSettings = {
+    authority: "https://identity.badeev.info",
+    client_id: "spa_badeev",
+    redirect_uri: "http://localhost:3000/auth-complete",
+    response_type: "code",
+    scope: "openid Portfolio.API",
+    post_logout_redirect_uri: "https://badeev.info",
+    userStore: new WebStorageStateStore({ store: window.localStorage }),
+}
+
+const getConfig = () => {
+    const hostname = window.location.hostname
+
+    if (hostname === "localhost" || hostname === "127.0.0.1") {
+        return devConfig
+    }
+
+    return prodConfig
+}
+
+const userManager = new UserManager(getConfig())
 
 export default class AuthService {
     @observable user: User | null = null
